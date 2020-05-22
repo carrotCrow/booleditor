f5867a5d8a2bff140455d054fecdb6a296d3b1b0
carrotCrow <carrotCrow@users.noreply.github.com>
2020-05-22 10:56:50 +0700

    add Navbar Component

---
+++ client/components/Navbar/menuStyle.js
@@ -0,0 +1,7 @@
+const menuStyle = "m-2 hover:text-blue-500 cursor-pointer"
+const dropdownStyle = "m-2 cursor-pointer"
+const userCard = "hover:text-blue-400 cursor-pointer"
+
+module.exports = {
+    menuStyle,userCard,dropdownStyle
+}
